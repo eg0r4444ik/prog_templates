9a64fd389bf62ca6a8121380635cecc4acbda593
Rogachev Egor <55750592+288801@users.noreply.github.com>
2023-10-06 13:59:47 +0300

    Update and rename DisjointSetSystem.java to DSU.java

---
+++ src/DSU.java
@@ -0,0 +1,36 @@
+static class DSU{
+        public static int[] p;
+        public static int[] r;
+
+        public DSU(int n) {
+            p = new int[n+1];
+            r = new int[n+1];
+        }
+
+        public static int find(int x){
+            if(x == p[x]){
+                return x;
+            }
+            p[x] = find(p[x]);
+            return p[x];
+        }
+
+        public static void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (r[x] > r[y]) {
+                int t = x;
+                x = y;
+                y = t;
+            }
+            if(x != y) {
+                r[y] += r[x];
+                p[x] = y;
+            }
+        }
+
+        public static boolean get(int x, int y){
+            return find(x) == find(y);
+        }
+
+    }
